0ad2a35ed5a8688962ed49a4267e4ea46e761457
khaledibrahim7 <khaledramadanza@gmail.com>
2024-10-14 19:47:28 +0300

    Java 8 Solution

---
+++ java8/src/main/java/org/example/LambdaExample.java
@@ -0,0 +1,163 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class LambdaExample {
+
+    public static void main(final String[] args) {
+
+        final UsersRepository repository = new UsersRepository();
+
+        banner("Listing all users");
+        // SOLVED all users
+        repository.select(null, null);
+
+        banner("Listing all active users");
+        // SOLVED With functional interfaces declared
+        Predicate<User> activeUserPredicate = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.active;
+            }
+        };
+        repository.select(activeUserPredicate, null);
+
+        banner("Listing all active users - lambda");
+        // SOLVED With functional interfaces used directly
+        repository.select(user -> user.active, null);
+
+//        -------------------------------------------------------------------
+        banner("Listing users with age > 5 sorted by name");
+        // Solved With functional interfaces declared
+        Predicate<User> greaterThan5AgePredicate = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.age > 5;
+            }
+        };
+
+        Comparator<User> sortedByNameComparator = new Comparator<User>() {
+            @Override
+            public int compare(User currentUser, User newUser) {
+                return currentUser.name.compareTo(newUser.name);
+            }
+        };
+        repository.select(greaterThan5AgePredicate, sortedByNameComparator);
+
+//          -------------------------------------------------------------------------------------
+        banner("Listing users with age > 5 sorted by name - lambda");
+        // Solved With functional interfaces used directly
+        repository.select(user -> user.age > 5, (currentUser, newUser) -> currentUser.name.compareTo(newUser.name));
+        System.out.println("============OR================");
+        repository.select(user -> user.age > 5, Comparator.comparing(currentUser -> currentUser.name));
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        banner("Listing users with age < 10 sorted by age");
+        // solved With functional interfaces declared
+        Predicate<User> UserlessThan10Predicate = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.age < 10;
+            }
+        };
+        Comparator<User> sortedByUserAgeComparator = new Comparator<User>() {
+            @Override
+            public int compare(User currentUser1, User newUser1) {
+                return Integer.compare(currentUser1.age, newUser1.age);
+            }
+        };
+        repository.select(UserlessThan10Predicate, sortedByUserAgeComparator);
+//       --------------------------------------------
+        banner("Listing users with age < 10 sorted by age - lambda");
+        ///////// // solved With functional interfaces used directly
+
+        repository.select(user -> user.age < 10, Comparator.comparing(currentUser -> currentUser.age));
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        banner("Listing active users sorted by name");
+        // solved With functional interfaces declared
+        Predicate<User> userActiveName = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.active;
+            }
+        };
+
+        repository.select(userActiveName, sortedByNameComparator);
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+        banner("Listing active users sorted by name - lambda");
+        ////// // solved With functional interfaces used directly
+        repository.select(user -> user.active, Comparator.comparing(currentUser -> currentUser.name));
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        banner("Listing active users with age > 8 sorted by name");
+        // Solved With functional interfaces declared
+        Predicate<User> userPredicate = new Predicate<User>() {
+            @Override
+            public boolean test(User user) {
+                return user.active && user.age > 8;
+            }
+        };
+
+        repository.select(userPredicate, sortedByNameComparator);
+
+        banner("Listing active users with age > 8 sorted by name - lambda");
+        // Solved With functional interfaces used directly
+        repository.select(user -> user.active && user.age > 8, Comparator.comparing(user -> user.name));
+    }
+
+    private static void banner(final String m) {
+        System.out.println("#### " + m + " ####");
+    }
+
+}
+
+class UsersRepository {
+    List<User> users;
+
+    UsersRepository() {
+        users = Arrays.asList(
+                new User("Seven", 7, true),
+                new User("Four", 4, false),
+                new User("Eleven", 11, true),
+                new User("Three", 3, true),
+                new User("Nine", 9, false),
+                new User("One", 1, true),
+                new User("Twelve", 12, true));
+    }
+
+    void select(final Predicate<User> filter, final Comparator<User> order) {
+        Stream<User> usersStream = users.stream();
+
+        if (filter != null) {
+            usersStream = usersStream.filter(filter);
+        }
+        if (order != null) {
+            usersStream = usersStream.sorted(order);
+        }
+        usersStream.forEach(System.out::println);
+    }
+}
+
+class User {
+    String name;
+    int age;
+    boolean active;
+
+    User(final String name, final int age, boolean active) {
+        this.name = name;
+        this.age = age;
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\t| " + age;
+    }
+}
