a3c724514412d7a3ea6ba22a149cc728ce863ade
khaled Ibrahim <125488658+khaledibrahim7@users.noreply.github.com>
2024-10-15 19:18:23 +0300

    Update LogManager.java

--- HomeSecuritySystemDp/src/main/java/example/Logs/LogManager.java
+++ HomeSecuritySystemDp/src/main/java/example/Logs/LogManager.java
@@ -3,10 +3,9 @@ package example.Logs;
 public class LogManager {
     private static LogManager instance;
 
-    // Private constructor to prevent instantiation
     private LogManager() {}
 
-    // Singleton instance method
+    
     public static LogManager getInstance() {
         if (instance == null) {
             instance = new LogManager();
@@ -14,9 +13,7 @@ public class LogManager {
         return instance;
     }
 
-    // Log method to write messages
     public void log(String message) {
-        // In a real application, this would append to a file.
         System.out.println("Log: " + message);
     }
 
