8defcd68cc474dca380f2f99089e8eed3c19a88d
khaled Ibrahim <125488658+khaledibrahim7@users.noreply.github.com>
2024-10-15 19:17:49 +0300

    Update MotionSensor.java

--- HomeSecuritySystemDp/src/main/java/example/sensors/MotionSensor.java
+++ HomeSecuritySystemDp/src/main/java/example/sensors/MotionSensor.java
@@ -7,7 +7,6 @@ public class MotionSensor implements Sensor {
     @Override
     public void detect() {
         System.out.println("Motion detected!");
-        // Log the detection
         LogManager.getInstance().log("Motion detected by MotionSensor.");
     }
 }
